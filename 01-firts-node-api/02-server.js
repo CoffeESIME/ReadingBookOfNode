ad4d81e33f80709ce7cd71a31d82be0fe4117732
CoffeESIME <fabian.romero@office365.3pillarglobal.com>
2021-10-19 12:44:30 -0500

    Json and text plain respond, basic routin

---
+++ 01-firts-node-api/02-server.js
@@ -0,0 +1,8 @@
+const http=require('http')
+const port=process.env.port || 1337
+const server=http.createServer(function(req,res){
+    res.setHeader('Content-Type','application/json')
+    res.end(JSON.stringify({text:'Hi',numbers:[1,2,3]}))
+})
+server.listen(port)
+console.log(`Server listening on port ${port}`)
